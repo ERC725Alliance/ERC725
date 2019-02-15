2818bf42f133edb8d368e50bfc9f6850c703629a
Fabian Vogelsteller <fabian.vogelsteller@gmail.com>
2019-02-15 14:05:22 -0700

    changed DataSet event and added ContractCreated event

--- contracts/contracts/ERC725.sol
+++ contracts/contracts/ERC725.sol
@@ -1,7 +1,8 @@
-pragma solidity ^0.5.1;
+pragma solidity ^0.5.4;
 
 interface ERC725 {
-    event DataSet(bytes32 indexed key, bytes32 indexed value);
+    event DataChanged(bytes32 indexed key, bytes32 indexed value);
+    event ContractCreated(address indexed contractAddress);
 
     function getData(bytes32 _key) external view returns (bytes32 _value);
     function setData(bytes32 _key, bytes32 _value) external;
