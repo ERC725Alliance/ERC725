bc3a883b8a0da65a6b7da56208c00a8d544dbd3b
Fabian Vogelsteller <fabian.vogelsteller@gmail.com>
2019-03-26 20:32:19 +0100

    added changeOwner

--- contracts/contracts/ERC725.sol
+++ contracts/contracts/ERC725.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.5.4;
 
 interface ERC725 {
     event DataChanged(bytes32 indexed key, bytes32 indexed value);
+    event OwnerChanged(address indexed ownerAddress);
     event ContractCreated(address indexed contractAddress);
 
+    function changeOwner(address _owner) external;
     function getData(bytes32 _key) external view returns (bytes32 _value);
     function setData(bytes32 _key, bytes32 _value) external;
     function execute(uint256 _operationType, address _to, uint256 _value, bytes calldata _data) external;
