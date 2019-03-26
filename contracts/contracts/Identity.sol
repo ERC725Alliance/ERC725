5223d7a5dd9e0224d928e47a3223470253980d9e
Fabian Vogelsteller <fabian.vogelsteller@gmail.com>
2019-03-26 20:31:44 +0100

    Updated implementation to include owner

--- contracts/contracts/Identity.sol
+++ contracts/contracts/Identity.sol
@@ -6,52 +6,55 @@ contract ProxyAccount is ERC725 {
     
     uint256 constant OPERATION_CALL = 0;
     uint256 constant OPERATION_CREATE = 1;
-    bytes32 constant KEY_OWNER = 0x0000000000000000000000000000000000000000000000000000000000000000;
 
     mapping(bytes32 => bytes32) store;
     
+    // the owner
+    address public owner;
+    
     
     constructor(address _owner) public {
-        store[KEY_OWNER] = toBytes32(_owner);
+        owner = _owner;
     }
-
 
     modifier onlyOwner() {
-        require(toBytes32(msg.sender) == store[KEY_OWNER], "only-owner-allowed");
+        require(msg.sender == owner, "only-owner-allowed");
         _;
-    }
-    
-    function toAddress(bytes32 a) internal pure returns (address b){
-       assembly {
-            mstore(0, a)
-            b := mload(0)
-        }
-       return b;
-    }
-    
-    function toBytes32(address a) internal pure returns (bytes32 b){
-       assembly {
-            mstore(0, a)
-            b := mload(0)
-        }
-       return b;
     }
     
     // ----------------
     // Public functions
     
     function () external payable {}
+    
+    function changeOwner(address _owner)
+        external
+        onlyOwner
+    {
+        owner = _owner;
+        emit OwnerChanged(owner);
+    }
 
-    function getData(bytes32 _key) external view returns (bytes32 _value) {
+    function getData(bytes32 _key)
+        external
+        view
+        returns (bytes32 _value)
+    {
         return store[_key];
     }
 
-    function setData(bytes32 _key, bytes32 _value) external onlyOwner {
+    function setData(bytes32 _key, bytes32 _value)
+        external
+        onlyOwner
+    {
         store[_key] = _value;
         emit DataChanged(_key, _value);
     }
 
-    function execute(uint256 _operationType, address _to, uint256 _value, bytes calldata _data) external onlyOwner {
+    function execute(uint256 _operationType, address _to, uint256 _value, bytes calldata _data)
+        external
+        onlyOwner
+    {
         if (_operationType == OPERATION_CALL) {
             executeCall(_to, _value, _data);
         } else if (_operationType == OPERATION_CREATE) {
