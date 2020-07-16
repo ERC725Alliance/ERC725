6040bacccfa873903a3c85a5df212992a0fe96d9
Fabian Vogelsteller <fabian@frozeman.de>
2020-07-16 11:21:04 +0200

    added relaytxaccepence to simple keymanager

--- implementations/contracts/helpers/SimpleKeyManager.sol
+++ implementations/contracts/helpers/SimpleKeyManager.sol
@@ -11,8 +11,11 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 // libraries
 import "@openzeppelin/contracts/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract SimpleKeyManager is ERC165, IERC1271, AccessControl {
+    using ECDSA for bytes32;
+    using SafeMath for uint256;
 
     bytes4 internal constant _INTERFACE_ID_ERC1271 = 0x1626ba7e;
     bytes4 internal constant _ERC1271FAILVALUE = 0xffffffff;
@@ -21,6 +24,7 @@ contract SimpleKeyManager is ERC165, IERC1271, AccessControl {
     bytes32 public constant EXECUTOR_ROLE = 0xd8aa0f3194971a2a116679f7c2090f6939c8d4e01a2a8d7e41d55e5351469e63;
 
     IERC725X public Account;
+    mapping (address => uint256) private _nonceStore;
 
     // EVENTS
     event Executed(uint256 indexed _operation, address indexed _to, uint256 indexed  _value, bytes _data);
@@ -39,11 +43,52 @@ contract SimpleKeyManager is ERC165, IERC1271, AccessControl {
     }
 
 
+    function getNonce(address _address)
+    public
+    view
+    returns (uint256) {
+        return _nonceStore[_address];
+    }
+
+
     function execute(uint256 _operationType, address _to, uint256 _value, bytes memory _data)
     external
     payable
     {
         require(hasRole(EXECUTOR_ROLE, _msgSender()), 'Only executors are allowed');
+
+        Account.execute(_operationType, _to, _value, _data);
+        emit Executed(_operationType, _to, _value, _data);
+    }
+
+
+    // allows anybody to execute given they have a signed messaged from an executor
+    function executeRelayedCall(
+        uint256 _operationType,
+        address _to,
+        uint256 _value,
+        bytes memory _data,
+        uint256 _nonce,
+        bytes memory _signature
+    )
+    external
+    {
+        bytes memory blob = abi.encodePacked(
+            _operationType,
+            _to,
+            _value,
+            _data,
+            _nonce // Prevents replays
+        );
+
+        // recover the signer
+        address from = keccak256(blob).toEthSignedMessageHash().recover(_signature);
+
+        require(hasRole(EXECUTOR_ROLE, from), 'Only executors are allowed');
+        require(_nonceStore[from] == _nonce, 'Incorrect nonce');
+
+        // increase the nonce
+        _nonceStore[from] = _nonceStore[from].add(1);
 
         Account.execute(_operationType, _to, _value, _data);
         emit Executed(_operationType, _to, _value, _data);
--- implementations/test/SimpleKeyManager.js
+++ implementations/test/SimpleKeyManager.js
@@ -5,7 +5,7 @@ const KeyManager = artifacts.require('SimpleKeyManager');
 
 // keccak256("EXECUTOR_ROLE")
 const EXECUTOR_ROLE = "0xd8aa0f3194971a2a116679f7c2090f6939c8d4e01a2a8d7e41d55e5351469e63";
-const DEFAULT_ADMIN_ROLE = "0x00";
+const DEFAULT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
 
 contract("SimpleKeyManager", async (accounts) => {
     let keyManager, account;
@@ -15,6 +15,9 @@ contract("SimpleKeyManager", async (accounts) => {
         account = await Account.new(owner, {from: owner});
         keyManager = await KeyManager.new(account.address, owner);
         await account.transferOwnership(keyManager.address, {from: owner});
+
+        // console.log(await keyManager.getRoleAdmin(EXECUTOR_ROLE));
+        // console.log(await keyManager.getRoleAdmin(DEFAULT_ADMIN_ROLE));
     });
 
     it('check if owners are correct', async function() {
@@ -22,6 +25,42 @@ contract("SimpleKeyManager", async (accounts) => {
         assert.isTrue(await keyManager.hasRole(DEFAULT_ADMIN_ROLE, owner));
         assert.isTrue(await keyManager.hasRole(EXECUTOR_ROLE, owner));
     });
+
+    it('should be able to add second owner', async function() {
+
+        // add owner
+        keyManager.grantRole(DEFAULT_ADMIN_ROLE, accounts[4], {from: owner});
+
+        assert.isTrue(await keyManager.hasRole(DEFAULT_ADMIN_ROLE, accounts[4]));
+    });
+
+    it('second owner should be be able to add executor', async function() {
+
+        // add owner
+        keyManager.grantRole(DEFAULT_ADMIN_ROLE, accounts[4], {from: owner});
+
+        // add executor
+        keyManager.grantRole(EXECUTOR_ROLE, accounts[5], {from: accounts[4]});
+
+        assert.isTrue(await keyManager.hasRole(EXECUTOR_ROLE, accounts[5]));
+    });
+
+    it('executor should not be able to add owner', async function() {
+
+        // add owner
+        keyManager.grantRole(DEFAULT_ADMIN_ROLE, accounts[4], {from: owner});
+
+        // add executor
+        keyManager.grantRole(EXECUTOR_ROLE, accounts[5], {from: accounts[4]});
+
+        // add owner
+        expectRevert(
+            keyManager.grantRole(EXECUTOR_ROLE, accounts[6], {from: accounts[5]}),
+            "AccessControl: sender must be an admin to grant"
+        );
+
+    });
+
 
     it('should be able to send value to the account and forward', async function() {
         let oneEth = web3.utils.toWei("1", 'ether');
@@ -57,6 +96,95 @@ contract("SimpleKeyManager", async (accounts) => {
                 keyManager.execute("0", accounts[2], oneEth, '0x00', {from: owner}),
                 "Only executors are allowed"
             );
+
+        assert.equal(await web3.eth.getBalance(account.address), oneEth);
+    });
+
+    it('using a signed relayExecution, should be able to send value to the account and forward', async function() {
+        let oneEth = web3.utils.toWei("1", 'ether');
+        web3.eth.accounts.wallet.create(1);
+        let emptyAccount = web3.eth.accounts.wallet[0];
+
+        await web3.eth.sendTransaction({
+            from: accounts[2],
+            to: account.address,
+            value: oneEth
+        });
+
+        assert.equal(await web3.eth.getBalance(account.address), oneEth);
+
+        // add a new executor
+        await keyManager.grantRole(EXECUTOR_ROLE, emptyAccount.address, {from: owner});
+
+        // sign execution
+        let nonce = await keyManager.getNonce(emptyAccount.address);
+        let signature = emptyAccount.sign(
+            web3.utils.soliditySha3(0, {t: 'address', v: accounts[2]}, oneEth, {t: 'bytes', v:'0x00'}, nonce)
+        );
+
+        // send from non-executor account, adding signed data
+        await keyManager.executeRelayedCall("0", accounts[2], oneEth, '0x00', nonce, signature.signature, {from: accounts[4]});
+
+        assert.equal(await web3.eth.getBalance(account.address), '0');
+    });
+
+    it('using a signed relayExecution, should fail wrong nonce send value if not executor', async function() {
+        let oneEth = web3.utils.toWei("1", 'ether');
+        web3.eth.accounts.wallet.create(1);
+        let emptyAccount = web3.eth.accounts.wallet[0];
+
+        await web3.eth.sendTransaction({
+            from: accounts[2],
+            to: account.address,
+            value: oneEth
+        });
+
+        assert.equal(await web3.eth.getBalance(account.address), oneEth);
+
+        // add a new executor
+        await keyManager.grantRole(EXECUTOR_ROLE, emptyAccount.address, {from: owner});
+
+        // sign execution
+        let nonce = await keyManager.getNonce(emptyAccount.address) + 2; // increase to much
+        let signature = emptyAccount.sign(
+            web3.utils.soliditySha3(0, {t: 'address', v: accounts[2]}, oneEth, {t: 'bytes', v:'0x00'}, nonce)
+        );
+
+        // send from non-executor account, adding signed data
+        await expectRevert(
+            keyManager.executeRelayedCall("0", accounts[2], oneEth, '0x00', nonce, signature.signature, {from: accounts[4]}),
+            "Incorrect nonce"
+        );
+
+        assert.equal(await web3.eth.getBalance(account.address), oneEth);
+    });
+
+    it('using a signed relayExecution, should fail to send value if not executor', async function() {
+        let oneEth = web3.utils.toWei("1", 'ether');
+        web3.eth.accounts.wallet.create(1);
+        let emptyAccount = web3.eth.accounts.wallet[0];
+
+        await web3.eth.sendTransaction({
+            from: accounts[2],
+            to: account.address,
+            value: oneEth
+        });
+
+        assert.equal(await web3.eth.getBalance(account.address), oneEth);
+
+        // add non new executor
+
+        // sign execution
+        let nonce = await keyManager.getNonce(emptyAccount.address);
+        let signature = emptyAccount.sign(
+            web3.utils.soliditySha3(0, {t: 'address', v: accounts[2]}, oneEth, {t: 'bytes', v:'0x00'}, nonce)
+        );
+
+        // send from non-executor account, adding signed data
+        await expectRevert(
+            keyManager.executeRelayedCall("0", accounts[2], oneEth, '0x00', 1, signature.signature, {from: accounts[4]}),
+            "Only executors are allowed"
+        );
 
         assert.equal(await web3.eth.getBalance(account.address), oneEth);
     });
