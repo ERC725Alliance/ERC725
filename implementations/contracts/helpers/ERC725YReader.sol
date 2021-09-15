f4dd67b5e5c5663bca89ebd21a0a46e5b6dcc986
mattgstevens <mattgstevens@gmail.com>
2021-09-15 10:35:12 +0000

    chore: rename test helper contracts for ERC725Y
    clarify the purpose of the two helper contracts

--- implementations/contracts/helpers/ERC725YReader.sol
+++ implementations/contracts/helpers/ERC725YReader.sol
@@ -3,11 +3,7 @@ pragma solidity ^0.8.0;
 
 import "../ERC725/IERC725Y.sol";
 
-contract Dummy1 {
-
-    function CallSetData(address to ,bytes32[] calldata _keys, bytes[] calldata _values) public {
-        IERC725Y(to).setData(_keys,_values);
-    }
+contract ERC725YReader {
 
     function CallGetData(address to ,bytes32[] calldata _keys) public view returns(bytes[] memory){
         return IERC725Y(to).getData(_keys);
