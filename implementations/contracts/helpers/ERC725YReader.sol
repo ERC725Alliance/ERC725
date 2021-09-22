b65f87070ed815f5638a607cf2dfaca776255152
YamenMerhi <yamennmerhi@gmail.com>
2021-09-22 10:32:54 +0300

    ERC725Y Helper Files

---
+++ implementations/contracts/helpers/ERC725YReader.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "../ERC725/IERC725Y.sol";
+
+contract ERC725YReader {
+
+    function CallGetData(address to, bytes32[] calldata _keys)
+        public
+        view
+        returns(bytes[] memory)
+    {
+        return IERC725Y(to).getData(_keys);
+    }
+
+}
