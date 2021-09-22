091b75bf577ba219f8aab88c9888b934d0ef7773
YamenMerhi <yamennmerhi@gmail.com>
2021-09-22 10:37:04 +0300

    ERC725X Helper File

---
+++ implementations/contracts/helpers/Return.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+contract ReturnTest {
+
+    struct Civiliens{
+        string name;
+        uint256 age;
+    }
+
+    struct Warriors{
+        bool dead;
+        uint256 num;
+    }
+
+    function functionThatRevertsWithError(string memory error) external pure {
+        revert(error);
+    }
+
+    function returnSomeBytes(bytes[] memory _someData,bytes[] memory _someData1,bytes[] memory _someData2) public pure returns(bytes[] memory,bytes[] memory,bytes[] memory) {
+        return (_someData,_someData1,_someData2);
+    }
+
+    function functionThatReturnsCiviliensAndWarriors(Civiliens[] memory _someData,Warriors[] memory _someData1) public pure returns(Civiliens[] memory, Warriors[] memory) {
+        return (_someData,_someData1);
+    }
+    
+}
