a68b8c3693e7e33692814dcdf1a833ec40a71954
CJ42 <cavallerajean@gmail.com>
2021-11-03 10:28:36 +0000

    refactor: add ERC725X/Y + ERC173 interface IDs in constants

--- implementations/contracts/InterfaceIDs.sol
+++ implementations/contracts/InterfaceIDs.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.0;
 
 // interfaces
-import "./interfaces/IERC1271.sol";
+import "./interfaces/IERC725X.sol";
+import "./interfaces/IERC725Y.sol";
 import "./interfaces/ILSP1_UniversalReceiver.sol";
 import "./interfaces/ILSP1_UniversalReceiverDelegate.sol";
+import "./interfaces/IERC1271.sol";
+import "./interfaces/IERC173.sol";
 
 // ERC725 - Smart Contract based Account
+bytes4 constant _INTERFACE_ID_ERC725X = type(IERC725X).interfaceId;
+bytes4 constant _INTERFACE_ID_ERC725Y = type(IERC725Y).interfaceId;
 bytes4 constant _INTERFACE_ID_ERC725ACCOUNT = 0x63cb749b;
 
 // LSP1 - Universal Receiver
@@ -15,3 +20,6 @@ bytes4 constant _INTERFACE_ID_LSP1DELEGATE = type(ILSP1Delegate).interfaceId; //
 
 // ERC1271 - Standard Signature Validation
 bytes4 constant _INTERFACE_ID_ERC1271 = type(IERC1271).interfaceId; // 0x1626ba7e
+
+// ERC173 - Contract Ownership Standard
+bytes4 constant _INTERFACE_ID_ERC173 = type(IERC173).interfaceId;
