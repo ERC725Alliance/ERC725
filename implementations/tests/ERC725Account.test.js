073e9bf33fe6ab91d3098cd59e09f39342bf0065
CJ42 <cavallerajean@gmail.com>
2021-11-04 18:42:51 +0000

    test: :white_check_mark: fix ERC725Account failing test

--- implementations/tests/ERC725Account.test.js
+++ implementations/tests/ERC725Account.test.js
@@ -42,13 +42,12 @@ let supportStandardsKey = [
 ];
 
 contract("ERC725", function (accounts) {
-  let owner, nonOwner;
+  let owner = accounts[0],
+    nonOwner = accounts[1];
+
   let Account, Counter;
 
   beforeEach(async function () {
-    owner = accounts[0];
-    nonOwner = accounts[1];
-
     // Deploy contracts
     await AccountContract.detectNetwork();
     Account = await AccountContract.new(accounts[0]);
@@ -142,11 +141,11 @@ contract("ERC725", function (accounts) {
 });
 
 contract.only("ERC725Account", (accounts) => {
-  let owner;
   let Account;
 
+  let owner = accounts[2];
+
   before(async function () {
-    owner = accounts[2];
     Account = await AccountContract.new(owner, { from: owner });
   });
 
@@ -268,9 +267,8 @@ contract.only("ERC725Account", (accounts) => {
     });
 
     it("Allows owner to execute calls", async () => {
-      const dest = accounts[6];
+      const recipient = accounts[6];
       const amount = ether("1");
-      const OPERATION_CALL = 0x0;
 
       await web3.eth.sendTransaction({
         from: owner,
@@ -278,19 +276,18 @@ contract.only("ERC725Account", (accounts) => {
         value: amount,
       });
 
-      //   const destBalance = await web3.eth.getBalance(dest);
+      const destBalance = await web3.eth.getBalance(recipient);
 
-      //   await Account.execute(OPERATION_CALL, dest, amount, "0x0", {
-      //     from: owner,
-      //   });
+      await Account.execute(OPERATION_CALL, recipient, amount, "0x0", {
+        from: owner,
+      });
 
-      //   const finalBalance = await web3.eth.getBalance(dest);
+      const finalBalance = await web3.eth.getBalance(recipient);
 
-      //   assert.isTrue(new BN(destBalance).add(amount).eq(new BN(finalBalance)));
+      assert.isTrue(new BN(destBalance).add(amount).eq(new BN(finalBalance)));
     });
 
     it("Should revert when calling a function that reverts", async () => {
-      const OPERATION_CALL = 0x0;
       returnTest = await ReturnTest.new({ from: owner });
       abi = returnTest.contract.methods
         .functionThatRevertsWithError("Yamen")
@@ -305,9 +302,8 @@ contract.only("ERC725Account", (accounts) => {
     });
 
     it("Fails with non-owner executing", async () => {
-      const dest = accounts[6];
+      const recipient = accounts[6];
       const amount = ether("1");
-      const OPERATION_CALL = 0x0;
 
       // send money to the Account
       await web3.eth.sendTransaction({
@@ -318,7 +314,7 @@ contract.only("ERC725Account", (accounts) => {
 
       // try to move it away
       await expectRevert(
-        Account.execute(OPERATION_CALL, dest, amount, "0x0", {
+        Account.execute(OPERATION_CALL, recipient, amount, "0x0", {
           from: newOwner,
         }),
         "Ownable: caller is not the owner"
@@ -326,13 +322,13 @@ contract.only("ERC725Account", (accounts) => {
     });
 
     it("Allows owner to execute create", async () => {
-      const dest = accounts[6];
+      const recipient = accounts[6];
       const amount = ether("1");
       const OPERATION_CREATE = 1;
 
       let receipt = await Account.execute(
         OPERATION_CREATE,
-        dest,
+        recipient,
         "0",
         "0x608060405234801561001057600080fd5b506040516105f93803806105f98339818101604052602081101561003357600080fd5b810190808051906020019092919050505080600160006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff16021790555050610564806100956000396000f3fe60806040526004361061003f5760003560e01c806344c028fe1461004157806354f6127f146100fb578063749ebfb81461014a5780638da5cb5b1461018f575b005b34801561004d57600080fd5b506100f96004803603608081101561006457600080fd5b8101908080359060200190929190803573ffffffffffffffffffffffffffffffffffffffff16906020019092919080359060200190929190803590602001906401000000008111156100b557600080fd5b8201836020820111156100c757600080fd5b803590602001918460018302840111640100000000831117156100e957600080fd5b90919293919293905050506101e6565b005b34801561010757600080fd5b506101346004803603602081101561011e57600080fd5b81019080803590602001909291905050506103b7565b6040518082815260200191505060405180910390f35b34801561015657600080fd5b5061018d6004803603604081101561016d57600080fd5b8101908080359060200190929190803590602001909291905050506103d3565b005b34801561019b57600080fd5b506101a46104df565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b600160009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff16146102a9576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260128152602001807f6f6e6c792d6f776e65722d616c6c6f776564000000000000000000000000000081525060200191505060405180910390fd5b600085141561030757610301848484848080601f016020809104026020016040519081016040528093929190818152602001838380828437600081840152601f19601f82011690508083019250505050505050610505565b506103b0565b60018514156103aa57600061035f83838080601f016020809104026020016040519081016040528093929190818152602001838380828437600081840152601f19601f8201169050808301925050505050505061051d565b90508073ffffffffffffffffffffffffffffffffffffffff167fcf78cf0d6f3d8371e1075c69c492ab4ec5d8cf23a1a239b6a51a1d00be7ca31260405160405180910390a2506103af565b600080fd5b5b5050505050565b6000806000838152602001908152602001600020549050919050565b600160009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614610496576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260128152602001807f6f6e6c792d6f776e65722d616c6c6f776564000000000000000000000000000081525060200191505060405180910390fd5b806000808481526020019081526020016000208190555080827f35553580e4553c909abeb5764e842ce1f93c45f9f614bde2a2ca5f5b7b7dc0fb60405160405180910390a35050565b600160009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b600080600083516020850186885af190509392505050565b60008151602083016000f0905091905056fea265627a7a723158207fb9c8d804ca4e17aec99dbd7aab0a61583b56ebcbcb7e05589f97043968644364736f6c634300051100320000000000000000000000009501234ef8368466383d698c7fe7bd5ded85b4f6",
         {
@@ -406,12 +402,15 @@ contract.only("ERC725Account", (accounts) => {
     });
   });
 
-  context.only("Using key manager as owner", async () => {
+  context("Using KeyManager as owner", async () => {
     let KeyManager;
     let Account;
-    const owner = accounts[6];
 
-    before(async () => {
+    let owner = accounts[6];
+
+    const dataToSign = "0xcafecafe";
+
+    beforeEach(async () => {
       Account = await AccountContract.new(owner, { from: owner });
       KeyManager = await SimpleKeyManager.new(Account.address, owner, {
         from: owner,
@@ -419,14 +418,13 @@ contract.only("ERC725Account", (accounts) => {
       await Account.transferOwnership(KeyManager.address, { from: owner });
     });
 
-    it("Account should have owner as manager", async () => {
+    it("Account should have owner as KeyManager", async () => {
       const accountOwner = await Account.owner.call();
       assert.equal(accountOwner, KeyManager.address, "Addresses should match");
     });
 
     context("ERC1271 from KeyManager", async () => {
-      it("Can verify signature from executor of keymanager", async () => {
-        const dataToSign = "0xcafecafe";
+      it("Can verify signature from executor of KeyManager", async () => {
         const signature = DUMMY_SIGNER.sign(dataToSign);
 
         // add new owner to keyManager
@@ -445,7 +443,8 @@ contract.only("ERC725Account", (accounts) => {
           "Should define the signature as valid"
         );
       });
-      it("Can verify signature from owner of keymanager", async () => {
+
+      it("Can verify signature from owner of KeyManager", async () => {
         Account = await AccountContract.new(owner, { from: owner });
         KeyManager = await SimpleKeyManager.new(
           Account.address,
@@ -456,7 +455,6 @@ contract.only("ERC725Account", (accounts) => {
         );
         await Account.transferOwnership(KeyManager.address, { from: owner });
 
-        const dataToSign = "0xcafecafe";
         const signature = DUMMY_SIGNER.sign(dataToSign);
 
         const result = await Account.isValidSignature.call(
@@ -471,7 +469,6 @@ contract.only("ERC725Account", (accounts) => {
         );
       });
       it("Should fail when verifying signature from not-owner", async () => {
-        const dataToSign = "0xcafecafe";
         const signature = DUMMY_SIGNER.sign(dataToSign);
 
         const result = await KeyManager.isValidSignature.call(
@@ -487,12 +484,11 @@ contract.only("ERC725Account", (accounts) => {
       });
     });
 
-    it("Key manager can execute on behalf of Identity", async () => {
-      const dest = accounts[1];
+    it("Key Manager can execute on behalf of Identity", async () => {
+      const recipient = accounts[1];
       const amount = ether("1");
-      const OPERATION_CALL = 0x0;
 
-      //   Fund Account contract
+      // Fund Account contract
       await web3.eth.sendTransaction({
         from: owner,
         to: Account.address,
@@ -500,12 +496,12 @@ contract.only("ERC725Account", (accounts) => {
       });
 
       // Initial Balances
-      const destBalance = await web3.eth.getBalance(dest);
+      const destBalance = await web3.eth.getBalance(recipient);
       const idBalance = await web3.eth.getBalance(Account.address);
       const managerBalance = await web3.eth.getBalance(KeyManager.address);
 
       let abi = Account.contract.methods
-        .execute(OPERATION_CALL, dest, amount.toString(), "0x00")
+        .execute(OPERATION_CALL, recipient, amount.toString(), "0x")
         .encodeABI();
 
       await KeyManager.execute(abi, {
@@ -513,7 +509,7 @@ contract.only("ERC725Account", (accounts) => {
       });
 
       //Final Balances
-      const destBalanceFinal = await web3.eth.getBalance(dest);
+      const destBalanceFinal = await web3.eth.getBalance(recipient);
       const idBalanceFinal = await web3.eth.getBalance(Account.address);
       const managerBalanceFinal = await web3.eth.getBalance(KeyManager.address);
 
