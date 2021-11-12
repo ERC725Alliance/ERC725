bc7f765250263d52006fcab3065ca8aecbde7c43
Fabian Vogelsteller <fabian@frozeman.de>
2021-11-12 13:10:18 +0100

    fix: add utils

---
+++ implementations/contracts/utils/ERC725Utils.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "../interfaces/IERC725Y.sol";
+
+library ERC725Utils {
+    function getDataSingle(IERC725Y _account, bytes32 _key) internal view returns (bytes memory) {
+        bytes32[] memory keys = new bytes32[](1);
+        keys[0] = _key;
+        bytes memory fetchResult = _account.getData(keys)[0];
+        return fetchResult;
+    }
+}
