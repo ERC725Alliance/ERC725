05ae69b3a8a4727fa734a258f2dc55de3c0f5488
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2021-12-22 08:55:49 +0000

    fix: initializer re-entrancy vulnerability from OZ (4.4.1) (#81)
    * fix: re-entrancy vulnerability from OZ (4.4.1)
    
    * chore(deps): bump @openzeppelin/contracts in /implementations (#78)
    
    Bumps [@openzeppelin/contracts](https://github.com/OpenZeppelin/openzeppelin-contracts) from 4.3.3 to 4.4.1.
    - [Release notes](https://github.com/OpenZeppelin/openzeppelin-contracts/releases)
    - [Changelog](https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/OpenZeppelin/openzeppelin-contracts/compare/v4.3.3...v4.4.1)
    
    ---
    updated-dependencies:
    - dependency-name: "@openzeppelin/contracts"
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- implementations/contracts/ERC725YInit.sol
+++ implementations/contracts/ERC725YInit.sol
@@ -17,7 +17,7 @@ contract ERC725YInit is ERC725YCore, Initializable {
      * @notice Sets the owner of the contract and register ERC725Y interfaceId
      * @param _newOwner the owner of the contract
      */
-    function initialize(address _newOwner) public virtual initializer {
+    function initialize(address _newOwner) public virtual onlyInitializing {
         // This is necessary to prevent a contract that implements both ERC725X and ERC725Y to call both constructors
         if (_newOwner != owner()) {
             OwnableUnset.initOwner(_newOwner);
