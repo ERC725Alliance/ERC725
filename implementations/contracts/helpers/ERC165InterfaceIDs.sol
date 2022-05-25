47b1e6beadd332799ed21048e0c006f6d79e2de5
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2022-05-25 13:48:21 +0100

    chore: fix + disable linter warnings (#122)
    * chore: disable linter warning for operation CREATE
    
    * chore: add revert reason string for interface ID checks
    
    * chore: disable + fix linter error in helpers
    
    * chore: disable linter in utils contracts

--- implementations/contracts/helpers/ERC165InterfaceIDs.sol
+++ implementations/contracts/helpers/ERC165InterfaceIDs.sol
@@ -13,12 +13,18 @@ import {_INTERFACEID_ERC725X, _INTERFACEID_ERC725Y} from "../constants.sol";
  */
 contract ERC165InterfaceIDs {
     function getERC725XInterfaceID() public pure returns (bytes4) {
-        require(_INTERFACEID_ERC725X == type(IERC725X).interfaceId);
+        require(
+            _INTERFACEID_ERC725X == type(IERC725X).interfaceId,
+            "hardcoded _INTERFACEID_ERC725X in `constants.sol` does not match `type(IERC725X).interfaceId`"    
+        );
         return _INTERFACEID_ERC725X;
     }
 
     function getERC725YInterfaceID() public pure returns (bytes4) {
-        require(_INTERFACEID_ERC725Y == type(IERC725Y).interfaceId);
+        require(
+            _INTERFACEID_ERC725Y == type(IERC725Y).interfaceId,
+            "hardcoded _INTERFACEID_ERC725Y in `constants.sol` does not match `type(IERC725Y).interfaceId`"
+        );
         return _INTERFACEID_ERC725Y;
     }
 }
--- implementations/contracts/utils/GasLib.sol
+++ implementations/contracts/utils/GasLib.sol
@@ -8,7 +8,7 @@ library GasLib {
     /**
      * @dev Will return unchecked incremented uint256
      */
-    function unchecked_inc(uint256 i) internal pure returns (uint256) {
+    function unchecked_inc(uint256 i) internal pure returns (uint256) { // solhint-disable func-name-mixedcase
         unchecked {
             return i + 1;
         }
