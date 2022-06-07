b5d63944c76434ef286a067748731724c9f9dcca
Yamen Merhi <yamennmerhi@gmail.com>
2022-06-07 15:43:07 +0300

    style: change nastpec wording (#130)

--- implementations/contracts/ERC725YCore.sol
+++ implementations/contracts/ERC725YCore.sol
@@ -16,11 +16,11 @@ import {OwnableUnset} from "./custom/OwnableUnset.sol";
 import {_INTERFACEID_ERC725Y} from "./constants.sol";
 
 /**
- * @title Core implementation of ERC725 Y General key/value store
+ * @title Core implementation of ERC725Y General data key/value store
  * @author Fabian Vogelsteller <fabian@lukso.network>
- * @dev Contract module which provides the ability to set arbitrary dataKey/dataValue sets that can be changed over time
- * It is intended to standardise certain dataKeys dataValue pairs to allow automated retrievals and interactions
- * from interfaces and other smart contracts
+ * @dev Contract module which provides the ability to set arbitrary data key/value pairs that can be changed over time
+ * It is intended to standardise certain data key/value pairs to allow automated read and writes
+ * from/to the contract storage
  */
 abstract contract ERC725YCore is OwnableUnset, ERC165, IERC725Y {
     /**
--- implementations/contracts/ERC725YInit.sol
+++ implementations/contracts/ERC725YInit.sol
@@ -5,11 +5,11 @@ pragma solidity ^0.8.0;
 import {ERC725YInitAbstract} from "./ERC725YInitAbstract.sol";
 
 /**
- * @title Deployable Proxy Implementation of ERC725 Y General key/value store
+ * @title Deployable Proxy Implementation of ERC725Y General data key/value store
  * @author Fabian Vogelsteller <fabian@lukso.network>
- * @dev Contract module which provides the ability to set arbitrary dataKey/dataValue sets that can be changed over time
- * It is intended to standardise certain dataKeys dataValue pairs to allow automated retrievals and interactions
- * from interfaces and other smart contracts
+ * @dev Contract module which provides the ability to set arbitrary data key/value pairs that can be changed over time
+ * It is intended to standardise certain data key/value pairs to allow automated read and writes
+ * from/to the contract storage
  */
 contract ERC725YInit is ERC725YInitAbstract {
     /**
