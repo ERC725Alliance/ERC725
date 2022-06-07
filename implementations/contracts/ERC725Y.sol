3a661a8075cc94d5a8504e369a043d09e978f758
YamenMerhi <yamennmerhi@gmail.com>
2022-06-07 12:40:42 +0300

    style: change nastpec wording

--- implementations/contracts/ERC725Y.sol
+++ implementations/contracts/ERC725Y.sol
@@ -7,11 +7,11 @@ import {OwnableUnset} from "./custom/OwnableUnset.sol";
 import {ERC725YCore} from "./ERC725YCore.sol";
 
 /**
- * @title ERC725 Y General key/value store
+ * @title ERC725Y General data key/value store
  * @author Fabian Vogelsteller <fabian@lukso.network>
- * @dev Contract module which provides the ability to set arbitrary dataKey/dataValue sets that can be changed over time
- * It is intended to standardise certain dataKeys dataValue pairs to allow automated retrievals and interactions
- * from interfaces and other smart contracts
+ * @dev Contract module which provides the ability to set arbitrary data key/value pairs that can be changed over time
+ * It is intended to standardise certain data key/value pairs to allow automated read and writes
+ * from/to the contract storage
  */
 contract ERC725Y is ERC725YCore {
     /**
