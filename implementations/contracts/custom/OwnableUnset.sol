906e12eef40012b4294ab3359c2732e99b8f21b1
CJ42 <cavallerajean@gmail.com>
2022-06-15 13:02:07 +0100

    refactor: mark ownership functions as `external`

--- implementations/contracts/custom/OwnableUnset.sol
+++ implementations/contracts/custom/OwnableUnset.sol
@@ -35,7 +35,7 @@ abstract contract OwnableUnset {
      * NOTE: Renouncing ownership will leave the contract without an owner,
      * thereby removing any functionality that is only available to the owner.
      */
-    function renounceOwnership() public virtual onlyOwner {
+    function renounceOwnership() external virtual onlyOwner {
         _setOwner(address(0));
     }
 
@@ -43,7 +43,7 @@ abstract contract OwnableUnset {
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
      * Can only be called by the current owner.
      */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
+    function transferOwnership(address newOwner) external virtual onlyOwner {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         _setOwner(newOwner);
     }
