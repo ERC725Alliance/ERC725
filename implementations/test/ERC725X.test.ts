568e90303342d3f29e8d94be4ad9f093b0d09aad
YamenMerhi <yamennmerhi@gmail.com>
2022-08-02 20:25:20 +0300

    test: refactor the test to use typescript and mocha

---
+++ implementations/test/ERC725X.test.ts
@@ -0,0 +1,143 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import {
+  ERC725X,
+  ERC725X__factory,
+  ERC725XInit,
+  ERC725XInit__factory,
+} from "../types";
+
+import {
+  getNamedAccounts,
+  shouldBehaveLikeERC725X,
+  shouldInitializeLikeERC725X,
+  ERC725XTestContext,
+} from "./ERC725X.behaviour";
+
+import { deployProxy } from "./fixtures";
+
+describe("ERC725X", () => {
+  describe("when using ERC725X contract with constructor", () => {
+    const buildTestContext = async (): Promise<ERC725XTestContext> => {
+      const accounts = await getNamedAccounts();
+
+      const deployParams = {
+        newOwner: accounts.owner.address,
+      };
+
+      const erc725X = await new ERC725X__factory(accounts.owner).deploy(
+        deployParams.newOwner
+      );
+
+      return { accounts, erc725X, deployParams };
+    };
+
+    describe("when deploying the contract", () => {
+      let context: ERC725XTestContext;
+
+      beforeEach(async () => {
+        context = await buildTestContext();
+      });
+
+      describe("when initializing the contract", () => {
+        shouldInitializeLikeERC725X(async () => {
+          const { erc725X, deployParams } = context;
+          return {
+            erc725X,
+            deployParams,
+            initializeTransaction: context.erc725X.deployTransaction,
+          };
+        });
+      });
+    });
+
+    describe("when testing deployed contract", () => {
+      shouldBehaveLikeERC725X(buildTestContext);
+    });
+  });
+
+  describe("when using ERC725X contract with proxy", () => {
+    const buildTestContext = async (): Promise<ERC725XTestContext> => {
+      const accounts = await getNamedAccounts();
+
+      const deployParams = {
+        newOwner: accounts.owner.address,
+      };
+
+      const erc725XBase = await new ERC725XInit__factory(
+        accounts.owner
+      ).deploy();
+
+      const erc725XProxy = await deployProxy(
+        erc725XBase.address,
+        accounts.owner
+      );
+      const erc725X = erc725XBase.attach(erc725XProxy);
+
+      return { accounts, erc725X, deployParams };
+    };
+
+    const initializeProxy = async (context: ERC725XTestContext) => {
+      return context.erc725X["initialize(address)"](
+        context.deployParams.newOwner
+      );
+    };
+
+    describe("when deploying the base implementation contract", () => {
+      it("prevent any address from calling the initialize(...) function on the implementation", async () => {
+        const accounts = await ethers.getSigners();
+
+        const erc725XBase = await new ERC725XInit__factory(
+          accounts[0]
+        ).deploy();
+
+        const randomCaller = accounts[1];
+
+        await expect(
+          erc725XBase["initialize(address)"](randomCaller.address)
+        ).to.be.revertedWith("Initializable: contract is already initialized");
+      });
+    });
+
+    describe("when deploying the contract as proxy", () => {
+      let context: ERC725XTestContext;
+
+      beforeEach(async () => {
+        context = await buildTestContext();
+      });
+
+      describe("when initializing the contract", () => {
+        shouldInitializeLikeERC725X(async () => {
+          const { erc725X, deployParams } = context;
+          const initializeTransaction = await initializeProxy(context);
+
+          return {
+            erc725X,
+            deployParams,
+            initializeTransaction,
+          };
+        });
+      });
+
+      describe("when calling initialize more than once", () => {
+        it("should revert", async () => {
+          await initializeProxy(context);
+
+          await expect(initializeProxy(context)).to.be.revertedWith(
+            "Initializable: contract is already initialized"
+          );
+        });
+      });
+    });
+
+    describe("when testing deployed contract", () => {
+      shouldBehaveLikeERC725X(() =>
+        buildTestContext().then(async (context) => {
+          await initializeProxy(context);
+
+          return context;
+        })
+      );
+    });
+  });
+});
