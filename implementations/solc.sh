90c8bf680ab0bff173a006a625e494437317dcc0
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2022-08-17 09:55:34 +0100

    ci: edit build & release workflows for hardhat (#154)
    * build: add hardhat-packager as dev dependency
    
    * ci: use `npm ci` for installing dependencies in build-test CI
    
    * build: add  script in  to compile contract
    
    * ci: edit dependencies installation, build and test steps in release ci
    
    * build: add hardhat packager configs
    
    * build: edit included artifacts list in npm package
    
    * build: add missing dependency for Hardhat Typescript
    
    * build: remove `npx` from npm scripts
    
    * build: add `clean` npm script
    
    * build: add script to analyse contracts with `solc` compiler features

--- implementations/solc.sh
+++
@@ -1,8 +0,0 @@
-rm -R ./solc/**/*
-solc --asm @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ ../Utils="$(pwd)/contracts/Utils" contracts/ERC725.sol -o ./solc/evm/
-solc --storage-layout @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ ../Utils="$(pwd)/contracts/Utils" contracts/ERC725.sol -o ./solc/storage-layout/ --pretty-json
-solc --opcodes @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ ../Utils="$(pwd)/contracts/Utils" contracts/ERC725.sol -o ./solc/opcodes/
-solc --hashes @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ ../Utils="$(pwd)/contracts/Utils" contracts/ERC725.sol -o ./solc/selectors/
-solc --gas @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ ../Utils="$(pwd)/contracts/Utils" contracts/ERC725.sol >> ./solc/gas-costs.md
-solc --bin @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ ../Utils="$(pwd)/contracts/Utils" contracts/ERC725.sol -o ./solc/bytecode/
-solc --metadata @openzeppelin/="$(pwd)"/node_modules/@openzeppelin/ solidity-bytes-utils/="$(pwd)"/node_modules/solidity-bytes-utils/ ../Utils="$(pwd)/contracts/Utils" contracts/ERC725.sol -o ./solc/metadata/
