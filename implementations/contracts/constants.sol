d4698cc22975bc3a62fd04f3101d400ca9c1daa8
CJ42 <cavallerajean@gmail.com>
2022-10-20 18:09:52 +0100

    docs: improve docs for operation type and contract creation

--- implementations/contracts/constants.sol
+++ implementations/contracts/constants.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 bytes4 constant _INTERFACEID_ERC725X = 0x44c028fe;
 bytes4 constant _INTERFACEID_ERC725Y = 0x714df77c;
 
-// ERC725X OPERATIONS TYPES
+// ERC725X OPERATION TYPES
 enum OPERATION_TYPE {
     CALL,
     CREATE,
--- implementations/contracts/interfaces/IERC725X.sol
+++ implementations/contracts/interfaces/IERC725X.sol
@@ -12,22 +12,22 @@ import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
  */
 interface IERC725X is IERC165 {
     /**
-     * @notice Emitted when a contract is created
-     * @param operation The operation used to create a contract
+     * @notice Emitted when deploying a contract
+     * @param operationType The opcode used to deploy the contract (CREATE or CREATE2)
      * @param contractAddress The created contract address
-     * @param value The amount of native tokens (in Wei) sent to the created contract address
+     * @param value The amount of native tokens (in Wei) sent to fund the created contract address
      */
     event ContractCreated(
-        uint256 indexed operation,
+        uint256 indexed operationType,
         address indexed contractAddress,
         uint256 indexed value
     );
 
     /**
-     * @notice Emitted when a contract executed.
-     * @param operationType The operation used to execute a contract
-     * @param to The address where the call is executed
-     * @param value The amount of native tokens transferred with the call (in Wei).
+     * @notice Emitted when calling an address (EOA or contract)
+     * @param operationType The low-level call opcode used to call the `to` address (CALL, STATICALL or DELEGATECALL)
+     * @param to The address to call whether an EOA or a contract
+     * @param value The amount of native tokens transferred with the call (in Wei)
      * @param selector The first 4 bytes (= function selector) of the data sent with the call
      */
     event Executed(
@@ -38,9 +38,9 @@ interface IERC725X is IERC165 {
     );
 
     /**
-     * @param operationType The operation to execute: CALL = 0; CREATE = 1; CREATE2 = 2; STATICCALL = 3; DELEGATECALL = 4
-     * @param to The smart contract or address to interact with (unused if a contract is created via operation 1 or 2)
-     * @param value The amount of native tokens to transfer (in Wei).
+     * @param operationType The operation type used: CALL = 0; CREATE = 1; CREATE2 = 2; STATICCALL = 3; DELEGATECALL = 4
+     * @param to The address of the EOA or smart contract to (unused if a contract is created via operation type 1 or 2)
+     * @param value The amount of native tokens to transfer (in Wei)
      * @param data The call data, or the bytecode of the contract to deploy
      *
      * @dev Generic executor function to:
@@ -55,8 +55,8 @@ interface IERC725X is IERC165 {
      * - if a `value` is provided, the contract MUST have at least this amount in its balance to execute successfully.
      * - `to` SHOULD be address(0) when deploying a contract.
      *
-     * Emits an {Executed} event, when a call is executed under `operationType` 0, 3 and 4
-     * Emits a {ContractCreated} event, when a contract is created under `operationType` 1 and 2
+     * Emits an {Executed} event, when a call is made with `operationType` 0 (CALL), 3 (STATICCALL) or 4 (DELEGATECALL)
+     * Emits a {ContractCreated} event, when deploying a contract with `operationType` 1 (CREATE) or 2 (CREATE2)
      */
     function execute(
         uint256 operationType,
