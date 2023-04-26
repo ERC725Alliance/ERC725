a2c90c6b5846a0990787d1a2f566f8d010f9ddce
YamenMerhi <yamennmerhi@gmail.com>
2023-04-26 16:57:29 +0300

    chore: fix natspec

--- implementations/contracts/errors.sol
+++ implementations/contracts/errors.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 /**
  * @dev reverts when trying to send more native tokens `value` than available in current `balance`.
  * @param balance the balance of the ERC725X contract.
- * @param value the amount of native tokens sent via `ERC725X.execute/Batch(...)`.
+ * @param value the amount of native tokens sent via `ERC725X.execute(...)`/`ERC725X.executeBatch(...)`.
  */
 error ERC725X_InsufficientBalance(uint256 balance, uint256 value);
 
@@ -16,31 +16,31 @@ error ERC725X_UnknownOperationType(uint256 operationTypeProvided);
 
 /**
  * @dev the `value` parameter (= sending native tokens) is not allowed when making a staticcall
- * via `ERC725X.execute/Batch(...)` because sending native tokens is a state changing operation.
+ * via `ERC725X.execute(...)`/`ERC725X.executeBatch(...)` because sending native tokens is a state changing operation.
  */
 error ERC725X_MsgValueDisallowedInStaticCall();
 
 /**
  * @dev the `value` parameter (= sending native tokens) is not allowed when making a delegatecall
- * via `ERC725X.execute/Batch(...)` because msg.value is persisting.
+ * via `ERC725X.execute(...)`/`ERC725X.executeBatch(...)` because msg.value is persisting.
  */
 error ERC725X_MsgValueDisallowedInDelegateCall();
 
 /**
- * @dev reverts when passing a `to` address while deploying a contract va `ERC725X.execute/Batch(...)`
+ * @dev reverts when passing a `to` address while deploying a contract va `ERC725X.execute(...)`/`ERC725X.executeBatch(...)`
  * whether using operation type 1 (CREATE) or 2 (CREATE2).
  */
 error ERC725X_CreateOperationsRequireEmptyRecipientAddress();
 
 /**
- * @dev reverts when contract deployment via `ERC725X.execute/Batch(...)` failed.
+ * @dev reverts when contract deployment via `ERC725X.execute(...)`/`ERC725X.executeBatch(...)` failed.
  * whether using operation type 1 (CREATE) or 2 (CREATE2).
  */
 error ERC725X_ContractDeploymentFailed();
 
 /**
  * @dev reverts when no contract bytecode was provided as parameter when trying to deploy a contract
- * via `ERC725X.execute/Batch(...)`, whether using operation type 1 (CREATE) or 2 (CREATE2).
+ * via `ERC725X.execute(...)`/`ERC725X.executeBatch(...)`, whether using operation type 1 (CREATE) or 2 (CREATE2).
  */
 error ERC725X_NoContractBytecodeProvided();
 
