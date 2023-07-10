445eec2af155c283833b3f053d9888ed294be640
CJ42 <cavallerajean@gmail.com>
2023-07-10 08:08:21 +0100

    docs: improve Natspec for interfaces

--- implementations/contracts/interfaces/IERC725X.sol
+++ implementations/contracts/interfaces/IERC725X.sol
@@ -5,17 +5,20 @@ pragma solidity ^0.8.0;
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 /**
- * @title The interface for ERC725X General executor
- * @dev ERC725X provides the ability to call arbitrary functions at any other smart contract and itself,
- * including using `delegatecall`, `staticcall`, as well as creating contracts using `create` and `create2`
- * This is the basis for a smart contract based account system, but could also be used as a proxy account system
+ * @title The interface for the ERC725X sub-standard, a generic executor
+ * @dev ERC725X provides the ability to call arbitrary functions on any other smart contract (including itself).
+ * It allows to use different type of message calls to interact with addresses such as `call`, `staticcall` and `delegatecall`.
+ * It also allows to deploy and create new contracts via both the `create` and `create2` opcodes.
+ * This is the basis for a smart contract based account system, but could also be used as a proxy account system.
  */
 interface IERC725X is IERC165 {
     /**
-     * @notice Emitted when deploying a contract
-     * @param operationType The opcode used to deploy the contract (CREATE or CREATE2)
-     * @param contractAddress The created contract address
-     * @param value The amount of native tokens (in Wei) sent to fund the created contract address
+     * @notice Deployed new contract at address `contractAddress` and funded with `value` wei (deployed using opcode: `operationType`).
+     * @dev Emitted when a new contract was created and deployed.
+     * @param operationType The opcode used to deploy the contract (`CREATE` or `CREATE2`).
+     * @param contractAddress The created contract address.
+     * @param value The amount of native tokens (in Wei) sent to fund the created contract on deployment.
+     * @param salt The salt used to deterministically deploy the contract (`CREATE2` only). If `CREATE` opcode is used, the salt value will be `bytes32(0)`.
      */
     event ContractCreated(
         uint256 indexed operationType,
@@ -25,11 +28,12 @@ interface IERC725X is IERC165 {
     );
 
     /**
-     * @notice Emitted when calling an address (EOA or contract)
-     * @param operationType The low-level call opcode used to call the `to` address (CALL, STATICALL or DELEGATECALL)
+     * @notice Called address `target` using `operationType` with `value` wei and `data`.
+     * @dev Emitted when calling an address `target` (EOA or contract) with `value`.
+     * @param operationType The low-level call opcode used to call the `target` address (`CALL`, `STATICALL` or `DELEGATECALL`).
      * @param target The address to call. `target` will be unused if a contract is created (operation types 1 and 2).
-     * @param value The amount of native tokens transferred with the call (in Wei)
-     * @param selector The first 4 bytes (= function selector) of the data sent with the call
+     * @param value The amount of native tokens transferred along the call (in Wei).
+     * @param selector The first 4 bytes (= function selector) of the data sent with the call.
      */
     event Executed(
         uint256 indexed operationType,
@@ -39,10 +43,12 @@ interface IERC725X is IERC165 {
     );
 
     /**
+     * @notice Calling address `target` using `operationType`, transferring `value` wei and data: `data`.
+     * 
      * @param operationType The operation type used: CALL = 0; CREATE = 1; CREATE2 = 2; STATICCALL = 3; DELEGATECALL = 4
      * @param target The address of the EOA or smart contract.  (unused if a contract is created via operation type 1 or 2)
      * @param value The amount of native tokens to transfer (in Wei)
-     * @param data The call data, or the creation bytecode of the contract to deploy
+     * @param data The call data, or the creation bytecode of the contract to deploy if `operationType` is `1` or `2`.
      *
      * @dev Generic executor function to:
      *
@@ -50,15 +56,15 @@ interface IERC725X is IERC165 {
      * - interact with any contract by passing an abi-encoded function call in the `data` parameter.
      * - deploy a contract by providing its creation bytecode in the `data` parameter.
      *
-     * Requirements:
+     * @custom:requiremements
+     * - SHOULD only be callable by the {owner} of the contract.
+     * - if a `value` is provided, the contract MUST have at least this amount to transfer to `target` from its balance and execute successfully.
+     * - if the operation type is `STATICCALL` (`3`) or `DELEGATECALL` (`4`), `value` transfer is disallowed and SHOULD be 0.
+     * - `target` SHOULD be `address(0)` when deploying a new contract via `operationType` `CREATE` (`1`), or `CREATE2` (`2`).
      *
-     * - SHOULD only be callable by the owner of the contract set via ERC173.
-     * - if a `value` is provided, the contract MUST have at least this amount in its balance to execute successfully.
-     * - if the operation type is STATICCALL or DELEGATECALL, `value` SHOULD be 0.
-     * - `target` SHOULD be address(0) when deploying a contract.
-     *
-     * Emits an {Executed} event, when a call is made with `operationType` 0 (CALL), 3 (STATICCALL) or 4 (DELEGATECALL)
-     * Emits a {ContractCreated} event, when deploying a contract with `operationType` 1 (CREATE) or 2 (CREATE2)
+     * @custom:events
+     * - {Executed} event when a call is made with `operationType` 0 (CALL), 3 (STATICCALL) or 4 (DELEGATECALL).
+     * - {ContractCreated} event when deploying a new contract with `operationType` 1 (CREATE) or 2 (CREATE2).
      */
     function execute(
         uint256 operationType,
@@ -68,27 +74,23 @@ interface IERC725X is IERC165 {
     ) external payable returns (bytes memory);
 
     /**
-     * @param operationsType The list of operations type used: CALL = 0; CREATE = 1; CREATE2 = 2; STATICCALL = 3; DELEGATECALL = 4
+     * @notice Calling multiple addresses `targets` using `operationsType`, transferring `values` wei and data: `datas`.
+     * 
+     * @dev Batch executor function that behaves the same as {execute} but allowing multiple operations in the same transaction.
+     * 
+     * @param operationsType The list of operations type used: `CALL = 0`; `CREATE = 1`; `CREATE2 = 2`; `STATICCALL = 3`; `DELEGATECALL = 4`
      * @param targets The list of addresses to call. `targets` will be unused if a contract is created (operation types 1 and 2).
-     * @param values The list of native token amounts to transfer (in Wei)
-     * @param datas The list of call data, or the creation bytecode of the contract to deploy
+     * @param values The list of native token amounts to transfer (in Wei).
+     * @param datas The list of calldata, or the creation bytecode of the contract to deploy if `operationType` is `1` or `2`.
      *
-     * @dev Generic batch executor function to:
+     * @custom:requirements
+     * - All the array parameters provided MUST be equal and have the same length.
+     * - SHOULD only be callable by the {owner} of the contract.
+     * - The contract MUST have in its balance **at least the sum of all the `values`** to transfer and execute successfully each calldata payloads.
      *
-     * - send native tokens to any address.
-     * - interact with any contract by passing an abi-encoded function call in the `datas` parameter.
-     * - deploy a contract by providing its creation bytecode in the `datas` parameter.
-     *
-     * Requirements:
-     *
-     * - The length of the parameters provided MUST be equal
-     * - SHOULD only be callable by the owner of the contract set via ERC173.
-     * - if a `values` is provided, the contract MUST have at least this amount in its balance to execute successfully.
-     * - if the operation type is STATICCALL or DELEGATECALL, `values` SHOULD be 0.
-     * - `targets` SHOULD be address(0) when deploying a contract.
-     *
-     * Emits an {Executed} event, when a call is made with `operationType` 0 (CALL), 3 (STATICCALL) or 4 (DELEGATECALL)
-     * Emits a {ContractCreated} event, when deploying a contract with `operationType` 1 (CREATE) or 2 (CREATE2)
+     * @custom:events
+     * - {Executed} event, when a call is made with `operationType` 0 (CALL), 3 (STATICCALL) or 4 (DELEGATECALL)
+     * - {ContractCreated} event, when deploying a contract with `operationType` 1 (CREATE) or 2 (CREATE2)
      */
     function executeBatch(
         uint256[] memory operationsType,
